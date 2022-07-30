519c87be9fd97c41a91f9305e197e8adffdd70e0
John Prado <jcprado20@icloud.com>
2022-07-30 14:20:00 -0400

    adds more corrections

--- src/Components/EditTransaction.js
+++ src/Components/EditTransaction.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL;
