711b9ee221a0c0ff5d67b0990d6182634aba2891
John Prado <jcprado20@icloud.com>
2022-07-30 23:33:19 -0400

    add functionality

--- src/Components/AccountTotal.js
+++ src/Components/AccountTotal.js
@@ -1,18 +1,10 @@
 import React from "react";
 import "./AccountTotal.css"
-const span = document.querySelector('#span')
+
 
 function AccountTotal({ transactions }) {
   let total = 0;
-  // const span = document.querySelector('#span')
 
-  // if(total < 0){
-  //   span.content.style.color = "red"
-  // }else if(total <= 1000){
-  //   span.content.style.color = "beige"
-  // }else{
-  //   span.content.style.color = "green"
-  // }
 
   let amounts = transactions.map((transaction) => Number(transaction.amount));
 
